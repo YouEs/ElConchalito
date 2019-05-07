48b4415aff5db61cfaa78d4994aa8cb8f1530cea
Yuossef_Esper <yuossef96@gmail.com>
2019-05-07 06:31:42 -0500

    Se creo la estructura del proyecto generada por yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elConchalitoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the elConchalitoApp
+ */
+angular.module('elConchalitoApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name elConchalitoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the elConchalitoApp
+ */
+angular.module('elConchalitoApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
